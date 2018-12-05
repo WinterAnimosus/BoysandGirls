63435026fad0a37952e603fb04ef1d304a76d68b
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2018-12-05 15:14:15 -0400

    set up

--- wp-content/themes/bgc/header.php
+++ wp-content/themes/bgc/header.php
@@ -12,5 +12,10 @@
 </head>
 <body>
     <nav>
-
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
     </nav>
