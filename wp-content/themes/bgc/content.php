2e0d986056afa54bea7c5c49ced571c6b0bee0e8
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2018-12-06 13:24:13 -0400

    setting stuff up!

--- wp-content/themes/bgc/content.php
+++ wp-content/themes/bgc/content.php
@@ -1,8 +1,14 @@
-<?php
-/*
-Template Name: content
-*/
-?>
+<?php /*Template Name: content*/ ?>
+<?php get_header(); ?>
 <main>
-<h1>test</h1>
+<?php
+if (have_posts()):
+  while (have_posts()) : the_post();
+    the_content();
+  endwhile;
+else:
+  echo '<p>Sorry, no posts matched your criteria.</p>';
+endif;
+?>
 </main>
+<?php get_footer();?>
