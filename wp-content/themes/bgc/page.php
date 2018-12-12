580ce2e3326dc3af04a438dfc78d5c975ff9de4c
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2018-12-12 13:46:03 -0400

    working on size of body and slider

--- wp-content/themes/bgc/page.php
+++ wp-content/themes/bgc/page.php
@@ -1,16 +1,22 @@
 <?php get_header(); ?>
 <main>
     <?php get_template_part('notice',get_post_format());?>
-    <div class="content">
+    
         <?php
+            if(''!==(get_post(98)->post_content)){
+                echo '<div class="content1">';
+            }else{
+                echo '<div class="content2">';
+            }
             if (have_posts()):
                 while (have_posts()) : the_post();
                     the_content();
                 endwhile;
                 else: echo '<p>Sorry, no posts matched your criteria.</p>';
             endif;
+            echo '</div>';
         ?>
-    </div>
+    
     <?php get_template_part('sponsers',get_post_format());?>
 </main>
 <?php get_footer();?>
--- wp-content/themes/bgc/sponsers.php
+++ wp-content/themes/bgc/sponsers.php
@@ -1,4 +1,5 @@
 <?php /*Template Name: sponsers*/ ?>
 <div class="sliding">
-<?php echo do_shortcode('[metaslider id="111"]'); ?>
+    <?php echo do_shortcode('[metaslider id="111"]'); ?>
+    <?php echo do_shortcode('[metaslider id="117"]'); ?>
 </div>
