f72989c1970a10bb292173786173325e1fb34a0d
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2018-12-13 15:33:59 -0400

    changed sponsers and added a hide

--- wp-content/themes/bgc/sponsers.php
+++ wp-content/themes/bgc/sponsers.php
@@ -1,5 +1,28 @@
 <?php /*Template Name: sponsers*/ ?>
-<div class="sliding">
+<script>
+    var hid = false;
+    function hide(){
+        if(hid == false)
+        {
+            document.getElementById("hide").innerHTML="Show <i class='arrow up'></i>";
+            hid = true;
+            document.getElementById("slide").style.display = "none";
+            document.getElementById("hide").style.cssText = "grid-row:12/13;";
+        }else if(hid == true){
+            document.getElementById("hide").innerHTML="hide <i class='arrow down'></i>";
+            hid = false;
+            document.getElementById("slide").style.display = "flex";
+            document.getElementById("hide").style.cssText = "grid-row:10/11;";
+        }
+    }
+</script>
+<button type="button" onclick="hide()"id="hide">hide <i class="arrow down"></i></button>
+<div class="sliding" id="slide">
+    <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
+    <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
+    <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
+    <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
+    <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
     <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
     <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
     <img src="<?php echo 'http://'.$_SERVER['HTTP_HOST']; ?>/bgc/wp-content/uploads/2018/12/wildcats.png">
