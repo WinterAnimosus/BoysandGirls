cf3a57740eafb52d3297474af5d4358a6a9c95ac
WinterAnimosus <WinterAnimosus_nathan@hotmail.com>
2019-01-14 09:32:06 -0400

    removed posts from calander thing

---
+++ wp-content/themes/bgc/page-Calander.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<main>
+    <?php get_template_part('notice',get_post_format());?>
+    
+        <?php
+            
+            // if (have_posts()):
+            //     while (have_posts()) : the_post();
+            //         the_content();
+            //     endwhile;
+            //     else: echo '<p>Sorry, no posts matched your criteria.</p>';
+            // endif;
+            echo '</div>';
+        ?>
+    
+    <?php get_template_part('sponsers',get_post_format());?>
+</main>
+<?php get_footer();?>
